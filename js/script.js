d9bd245c859a3eb4138100d8e81caed8b12b6322
Sebastian_Wlo <sebastianwlodarczyk92@gmail.com>
2025-06-18 01:49:35 +0200

    Extended input function to gather operation sign and the second numeric value

--- js/script.js
+++ js/script.js
@@ -1,22 +1,41 @@
 const numbersDisplay = document.querySelector("#numbers-display");
 
 const numBtnsNodeList = document.querySelectorAll(".num-button");
-const numBtnsList = Array.from(numBtnsNodeList)
+const numBtnsList = Array.from(numBtnsNodeList);
+const opBtnsNodeList = document.querySelectorAll(".op-button");
+const opBtnsList = Array.from(opBtnsNodeList);
 
 let inputs = {
-  a: "",
+  a: "0",
   operator: null,
-  b: null,
+  b: "0",
 };
 
+const operations = {
+  addition: (a, b) => {return a + b},
+  subtraction: (a, b) => {return a - b},
+}
+const opKeys = {
+  "+": "addition",
+  "-": "subtraction",
+  "*": "multiplication",
+  "/": "division"
+}
+
 function checkKey(e) {
-  console.log("rawInput", e.key)
-  getInput(e.key);
-  document.querySelector(`#btn-${e.key}`).classList.add("pressed");
+  let keyValue = e.key;
+  for (let operation of Object.keys(opKeys)) {
+    if (keyValue === operation) {
+      keyValue = opKeys[operation];
+      console.log(keyValue)
+    }
+  }
+  getInput(keyValue);
 };
 
 function initialSetup() {
   numbersDisplay.innerText = "0";
+
   // Add eventListeners to Number inputs
   for (let btn of numBtnsList) {
     btn.addEventListener("click", (e) => {
@@ -24,19 +43,44 @@ function initialSetup() {
       getInput(buttonVal);
       })
   }
+
+  //Add eventListeners to Operation inputs
+  for (let btn of opBtnsList) {
+    btn.addEventListener("click", (e) => {
+      const buttonVal = e.target.id.replace("btn-", "");
+      getInput(buttonVal)
+    })
+  }
 };
 
 function getInput(val) {
   const parsedVal = parseInt(val);
   if (parsedVal || parsedVal === 0) {
     if (!inputs.operator && inputs.a.length < 9) {
-      console.log("Int:", parsedVal);
-      inputs.a += parsedVal;
+      console.log("Int 1:", parsedVal);
+      if (inputs.a !== "0"){
+        inputs.a += parsedVal;
+      } else {
+        inputs.a = String(parsedVal);
+      }
       numbersDisplay.innerText = inputs.a
+    } else if (inputs.operator && inputs.b.length < 9) {
+      console.log("Int 2:", parsedVal);
+      if (inputs.b !== "0"){
+        inputs.b += parsedVal;
+      } else {
+        inputs.b = String(parsedVal);
+      }
+      numbersDisplay.innerText = inputs.b;
     }
-  }
-  else {
-    console.log("Not Int! Value:", val);
+  } else {
+    for (let operation of Object.keys(operations)) {
+      if (val === operation) {
+        inputs.operator = val;
+        console.log("operation detected:", val, operation, "added operation:", inputs)
+      }
+    }
+    //console.log("Not Int! Value:", val);
   }
 };
 
