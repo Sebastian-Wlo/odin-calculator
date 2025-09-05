9390de9924eab993ba9b85722a9d2d717d1acf04
Sebastian_Wlo <sebastianwlodarczyk92@gmail.com>
2025-09-06 01:37:59 +0200

    refactor: modify script to follow the flowchart
    This refactors the script to follow the operations flowchart.
    (note: add the flowchart and update README.md

--- js/script.js
+++ js/script.js
@@ -1,140 +1,142 @@
 const numbersDisplay = document.querySelector("#numbers-display");
-
 const numBtnsNodeList = document.querySelectorAll(".btn-num");
 const numBtnsList = Array.from(numBtnsNodeList);
-console.log(numBtnsList)
-const opBtnsNodeList = document.querySelectorAll(".btn-op");
-const opBtnsList = Array.from(opBtnsNodeList);
-const equalsBtn = document.querySelector("#btn-equals");
 const clearBtn = document.querySelector("#on-ce");
-const decimalBtn = document.querySelector("#btn-dot");
 
 let inputs = {
-  a: "0",
-  operator: null,
-  b: "0",
+  a: "",
+  operator: "",
+  b: "",
 };
 
+let executed = false;
+
 const operations = {
-  addition: (a, b) => {return a + b},
-  subtraction: (a, b) => {return a - b},
-  multiplication: (a, b) => {return a * b},
+  addition: (a, b) => {
+    return (+a + +b).toString();
+  },
+  subtraction: (a, b) => {
+    return (+a - +b).toString();
+  },
+  multiplication: (a, b) => {
+    return (+a * +b).toString();
+  },
   division: (a, b) => {
-    if (b === 0) {
+    if (+b === 0) {
       return "nope!";
     } else {
-      return a / b;
+      return (+a / +b).toString();
     }
   },
-}
+};
+
 const opKeys = {
   "+": "addition",
   "-": "subtraction",
   "*": "multiplication",
   "/": "division",
-}
-
-function checkKey(e) {
-  let keyValue = e.key;
-  for (let operation of Object.keys(opKeys)) {
-    if (keyValue === operation) {
-      keyValue = opKeys[operation];
-      console.log(keyValue)
-    }
-  }
-  getInput(keyValue);
 };
 
-const clearAll = (a = "0", b = "0", operator = null) => {
-  numbersDisplay.innerText = "0";
-  inputs = {
-    a,
-    operator,
-    b,
-  }
+function executeOperation() {
+  let a = inputs.a === "" ? "0" : inputs.a;
+  let b = inputs.b === "" ? "0" : inputs.b;
+  let op = inputs.operator === "" ? "+" : inputs.operator;
+  let returnVal = operations[opKeys[op]](a, b);
+  displayValue(returnVal);
+  return returnVal;
 }
 
-function initialSetup() {
-  clearAll();
+clearBtn.addEventListener("click", (e) => clearAll());
 
-  // Add eventListeners to Number inputs
+// Initialize the calculator
+function init() {
   for (let btn of numBtnsList) {
-    btn.addEventListener("click", (e) => {
-      const buttonVal = e.target.id.replace("btn-", "");
-      getInput(buttonVal);
-      })
+    btn.addEventListener("click", () => readInput(btn.id.substring(4)));
   }
 
-  //Add eventListeners to Operation inputs
-  for (let btn of opBtnsList) {
-    btn.addEventListener("click", (e) => {
-      const buttonVal = e.target.id.replace("btn-", "");
-      getInput(buttonVal)
-    })
-  }
-};
+  clearAll();
+}
+init();
 
-function getInput(val) {
-  const parsedVal = parseInt(val);
-  console.log(parsedVal, val)
-  if (parsedVal || parsedVal === 0 || val === '.') {
-    if (!inputs.operator && inputs.a.length < 9) {
-      console.log("Int 1:", parsedVal);
-      if (val === '.' && inputs.a % 1 === 0) {
-        inputs.a += '.'
-        return
-      } else if (val === '.' && inputs.a % 1 !== 0) {
-        return
-      }
-      if (inputs.a !== "0"){
-        inputs.a += parsedVal;
-      } else {
-        inputs.a = String(parsedVal);
-      }
-      numbersDisplay.innerText = inputs.a
-    } else if (inputs.operator && inputs.b.length < 9) {
-      console.log("Int 2:", parsedVal);
-      if (val === '.' && inputs.b % 1 === 0) {
-        inputs.b += '.'
-        return
-      } else if (val === '.' && inputs.b % 1 !== 0) {
-        return
-      }
-      if (inputs.b !== "0"){
-        inputs.b += parsedVal;
-      } else {
-        inputs.b = String(parsedVal);
-      }
-      numbersDisplay.innerText = inputs.b;
+window.addEventListener("keydown", (e) => {
+  if (e.key.match(/^[\d\+\-\*\\]$/)) {
+    readInput(e.key);
+  } else if (e.key.match(/^[.,]$/)) {
+    readInput(".");
+  } else if (e.key === "Enter") {
+    readInput("=");
+  } else if (e.key === "Backspace") {
+    clearAll();
+  }
+});
+
+function readInput(char) {
+  if (char.match(/[\d\.,]/)) {
+    if (executed) {
+      clearAll();
+      inputs.a = numToValue(char, inputs.a);
+    } else if (inputs.operator === "") {
+      inputs.a = numToValue(char, inputs.a);
+      executed = false;
+    } else {
+      inputs.b = numToValue(char, inputs.b);
+      executed = false;
     }
+  } else if (char === "=") {
+    inputs.a = executeOperation();
+    executed = true;
+  } else if (executed) {
+    inputs.operator = char;
+    inputs.b = "";
+    executed = false;
+  } else if (inputs.b === "") {
+    inputs.operator = char;
   } else {
-    for (let operation of Object.keys(operations)) {
-      if (val === operation) {
-        inputs.operator = val;
-        console.log("operation detected:", val, operation, "added operation:", inputs)
-      }
-    }
-    //console.log("Not Int! Value:", val);
+    inputs.operator = char;
+    inputs.a = executeOperation();
+    inputs.b = "";
   }
-};
+  console.log(inputs.a, inputs.b, inputs.operator);
+}
 
-const executeOperation = (inputs) => {
-  const firstNumber = +inputs.a;
-  const secondNumber = +inputs.b;
-  const operation = inputs.operator
-  const result = operations[operation](firstNumber, secondNumber);
-  inputs.a = result !== "nope!" ? String(result) : "0";
-  numbersDisplay.innerText = result !== "nope!" ? inputs.a : result;
-  inputs.b = "0";
-  inputs.operator = null;
-  console.log("current inputs:", inputs);
-};
+function clearAll() {
+  executed = false;
+  inputs = {
+    a: "",
+    operator: "",
+    b: "",
+  };
+  displayValue(inputs.a);
+}
 
-equalsBtn.addEventListener("click", () => executeOperation(inputs));
-clearBtn.addEventListener("click", () => clearAll());
+function numToValue(char, input) {
+  let returnVal = "";
+  if (char === "." || char === ",") {
+    if (input === "") {
+      returnVal = "0.";
+    } else if (input.indexOf(".") === -1) {
+      returnVal = input + ".";
+    } else {
+      returnVal = input;
+    }
+  } else if (input === "0" || input === "") {
+    returnVal = char;
+  } else {
+    returnVal = input + char;
+  }
+  displayValue(returnVal);
+  return handleStrLen(returnVal);
+}
 
-window.addEventListener("keydown", checkKey);
+function displayValue(val) {
+  if (val === "") {
+    numbersDisplay.innerText = 0;
+  } else {
+    numbersDisplay.innerText = handleStrLen(val);
+  }
+}
 
-//numbersDisplay.innerText = "NOPE!";
-
-window.onload = () => initialSetup();
+function handleStrLen(value) {
+  let maxLength = value.includes(".") ? 11 : 10;
+  return value.length >= maxLength ? value.substring(0, maxLength) : value;
+}
